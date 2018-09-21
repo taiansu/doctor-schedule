e2078e4683eeb220cfe2a3bd2d839cc1d9ac4a50
Poligen <poligen@gmail.com>
2018-09-21 15:08:24 +0800

    setting up before calculation

--- lib/schedule/calculate.ex
+++ lib/schedule/calculate.ex
@@ -2,18 +2,23 @@ defmodule Schedule.Calculate do
   alias Schedule.MonthServer
   alias Schedule.PeopleServer
 
+  import IEx
+
   @moduledoc"""
   - first, setting up the month
   - setting up all the doctors(a string or a list, pipe into the map with id)
   - setting the points for everyone
   - start calculating from the day1
     - holidays first for all people
+    - if all holidays shared by all people then it is done;
+      if there is an extra one//pick from r1/r2
     - then arrange all people with friday
     - calculate all the rest day
   - if hit an error, restart again; if restart 100 times, show it was wrong
   - if it all success, show success, and the result
   """
 
+  #month setup
   def set_this_month(date, holidays \\ []) do
     GenServer.call(MonthServer, {:set_start, date, holidays})
   end
@@ -22,10 +27,20 @@ defmodule Schedule.Calculate do
     GenServer.call(MonthServer, {:get})
   end
 
+  #people setup
+  def get_current_people() do
+    GenServer.call(PeopleServer, {:get})
+  end
+
   def add_new_person(name, id, level, points) do
     GenServer.cast(PeopleServer, {:add, name, id, level, points})
   end
 
+  def set_reserve(id, weekdays \\ [], reserve_days \\ []) do
+    GenServer.cast(PeopleServer, {:set_reserve, id, weekdays, reserve_days })
+  end
+
+  #start calculate
   def set_the_holiday(month, people_list) do
     """
       random pick a person
--- lib/schedule/people_server.ex
+++ lib/schedule/people_server.ex
@@ -1,6 +1,7 @@
 defmodule Schedule.PeopleServer do
 use GenServer
 alias Schedule.Person
+import IEx
 
 def start_link() do
   GenServer.start_link(__MODULE__, nil, name: __MODULE__)
@@ -10,9 +11,26 @@ def init(_) do
   {:ok, %{}}
 end
 
+def handle_call({:get}, _from, people) do
+  {:reply, people, people}
+end
+
 def handle_cast({:add, name, id, level, points}, people) do
-  person = %Person{name: name, id: id, level: level, max_points: points}
+  person = %Person{
+    name: name,
+    id: id,
+    level: level,
+    max_points: points
+  }
   {:noreply, Map.put(people, id, person) }
+end
+
+def handle_cast({:set_reserve,id, weekdays, reserve_days}, people) do
+  person = Map.get(people, id) 
+  new_person = %{ person | reserve_days: reserve_days,
+                  weekday_reserve: weekdays
+                }
+  {:noreply, Map.put(people, id, new_person) }
 end
 
 end
